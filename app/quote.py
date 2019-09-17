12e056a71353f2968e1d8410117612ce34511078
Joe Ziems <jziems@yahoo.com>
2019-09-17 08:58:13 -0400

    removing unused imports

--- app/quote.py
+++ app/quote.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import random
-from io import BytesIO
 
 class Quotes:
 
@@ -18,15 +17,6 @@ class Quotes:
             html=requests.get('http://www.imdb.com/title/tt0080339/trivia?tab=qt&ref_=tt_trv_qu', headers={'User-Agent': 'Mozilla'})
             soup=BeautifulSoup(html.text, 'html.parser')
             for div in soup.findAll('div', {"class": "sodatext"}):
-                # buf = BytesIO()
-                # for p in div.findAll('p'):
-                #     if p:
-                #         character = p.find('span', {"class": "character"})
-                #         if character:
-                #             buf.write("{0}: {1} ".format(character.text, p.text.strip()[len(character.text)+2:]).encode())
-                #         else:
-                #             buf.write(" {0} ".format(p.text.strip()).encode())
-                # self.quotes.append(buf.getvalue().decode())
                 self.quotes.append(repr(div))
             with open('quotes.json', 'w') as fout:
                 json.dump(self.quotes, fout, indent=4)
